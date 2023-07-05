4401ccd3ed0c6e3764341dd4737322b2617a6425
Lucas Van Lume Lima <lvll@WINDOWS.CIN.UFPE.BR>
2023-07-05 15:02:39 -0300

    setup: cypress with cucumber

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    specPattern: "cypress/e2e/**/*.feature",
+    //specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173'
   }
 })
